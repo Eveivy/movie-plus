b5140e43b8a1e4a97074f4ca84fe82605a4be6b4
Isaac Evelyn <evelyn.isaac@cinfores.com>
2022-12-17 01:02:50 +1300

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import './App.css';
+import logo from './images/logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-p p-md-3 py-4 container-fluid text-white">
+      <div className="app">
+        <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+          <div class="container-fluid">
+            <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-brand">
+              <img src={logo} alt="logo" className='logo'/>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0 mt-3 mt-md-0">
+                <li class="nav-item">
+                  <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#">Link</a>
+                </li> 
+              </ul> 
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
